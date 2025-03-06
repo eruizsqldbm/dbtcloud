6e146a15bfffcb78ac32542c76f926ef5a630aa7
Eric Ruiz <ruizeric853@gmail.com>
2025-03-06 21:45:04 +0000

    fgfg

---
+++ models/Staging/STG_REGION.sql
@@ -0,0 +1,21 @@
+
+
+region_source AS (
+    SELECT
+        REGIONKEY,
+        TRIM(NAME) AS REGION_NAME,
+        UPPER(TRIM(COMMENT)) AS REGION_COMMENT,
+        CURRENT_TIMESTAMP AS LOAD_TIMESTAMP
+    FROM {{ source('TPCH_SF1', 'REGION') }}
+),
+
+stg_region1 AS (
+    SELECT
+        REGIONKEY,
+        REGION_NAME,
+        REGION_COMMENT,
+        LOAD_TIMESTAMP
+    FROM region_source
+)
+SELECT *
+FROM stg_region
